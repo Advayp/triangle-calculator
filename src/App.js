e327abbbb272dbed77c07fffded95f366741df52
Advay Patil <advaypatil27@gmail.com>
2022-10-25 22:16:23 -0700

    finish prompt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import { ChakraProvider, Box, theme } from '@chakra-ui/react';
+import React, { useRef } from 'react';
+import { ChakraProvider, theme, Box, VStack, Button } from '@chakra-ui/react';
+import { Navbar } from './components/Navbar';
+import { Prompt } from './components/Prompt';
 
 function App() {
+  // probably not ideal
+  const firstLength = useRef(0);
+  const secondLength = useRef(0);
+  const thirdLength = useRef(0);
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <h1>Hello, World</h1>
+      <Navbar />
+      <Box textAlign="center" fontSize="xl" w={'80vw'} mx="auto" mt={5}>
+        <VStack spacing={5}>
+          <Prompt label={'Side Length #1'} valueRef={firstLength} />
+          <Prompt label={'Side Length #2'} valueRef={secondLength} />
+          <Prompt label={'Side Length #3'} valueRef={thirdLength} />
+          <Button
+            colorScheme={'telegram'}
+            onClick={() => {
+              console.log(thirdLength.current);
+            }}
+          >
+            Calculate Angles
+          </Button>
+        </VStack>
       </Box>
     </ChakraProvider>
   );
---
+++ src/components/Prompt.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Input, Text, HStack } from '@chakra-ui/react';
+
+export const Prompt = ({ label, valueRef }) => {
+  return (
+    <>
+      <HStack spacing={3}>
+        <Text minW={'max-content'} fontSize={'md'}>
+          {label}
+        </Text>
+        <Input
+          placeholder="Enter a positive number"
+          size="md"
+          type="number"
+          onChange={event => {
+            valueRef.current = event.target.value;
+          }}
+        ></Input>
+      </HStack>
+    </>
+  );
+};
