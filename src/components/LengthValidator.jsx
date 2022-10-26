40f4248782b96d7f0ff5205713c5449a7d3885e1
Advay Patil <advaypatil27@gmail.com>
2022-10-25 23:16:00 -0700

    slight improvement to validation function

--- src/components/LengthValidator.jsx
+++ src/components/LengthValidator.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Text } from '@chakra-ui/react';
-import { isValid } from '../utils';
+import { isValid, compareNumbers } from '../utils';
 
 export const LengthValidator = ({ firstLength, secondLength, thirdLength }) => {
   const arrayFormat = [firstLength, secondLength, thirdLength].sort(
     compareNumbers
   );
 
-  function compareNumbers(a, b) {
-    return a - b;
-  }
-
   const { value, message } = isValid(arrayFormat);
 
   return (
-    <Text fontSize="xl" color={value ? 'green' : 'red'}>
+    <Text fontSize="xl" color={value ? 'green' : 'red.500'}>
       {message}
     </Text>
   );
